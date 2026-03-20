1fe55e39b9411192d3abbd4f6c822d093097e99e
aneeshchandra999 <aneeshchandra999@gmail.com>
2026-03-21 01:35:55 +0530

    Manage Loan Changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ import PocList from "@/pages/pocs/PocList"
 import UserLedgerTransactions from "./pages/ledgerBalances/UserLedgerTransactions"
 import CenterList from "./pages/center/CenterList"
 import StaffList from "./pages/staff/StaffList"
+import ManageLoanList from "./pages/loan/ManageLoanList"
+import LoanSchedulerList from "./pages/loanScheduler/LoanSchedulerList"
 
 function App() {
   return (
@@ -53,7 +55,8 @@ function App() {
           <Route path="staff" element={<StaffList />} />
           <Route path="members" element={<Placeholder />} />
           <Route path="loans/add" element={<Placeholder />} />
-          <Route path="loans/manage" element={<Placeholder />} />
+          <Route path="loans/manage" element={<ManageLoanList />} />
+          <Route path="loans/:loanId/scheduler" element={<LoanSchedulerList />} />
           <Route path="recovery-posting" element={<Placeholder />} />
           <Route path="ledger-transactions/:userId" element={<UserLedgerTransactions />} />
         </Route>
